61d906aebe628dc90b06ee833787878b545fa64d
thapanabin <thapanabincr7@outlook.com>
2019-10-26 10:26:59 -0500

    creating resources

--- src/main/java/com/nabin/springbootstarter/topic/TopicService.java
+++ src/main/java/com/nabin/springbootstarter/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.nabin.springbootstarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,12 +9,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 	
-	private List<Topic> topics = Arrays.asList(
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic("spring","Spring Framework","Sring"),
 			new Topic("java","java EE Framework","java"),
 			new Topic("hibernate","hibernate Framework","hibernate")
 			
-			);
+			));
 	
 	public List<Topic> getAllTopics(){
 		
@@ -22,6 +23,10 @@ public class TopicService {
 	
 	public Topic getTopic(String id) {
 		return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
 	}
 
 }
