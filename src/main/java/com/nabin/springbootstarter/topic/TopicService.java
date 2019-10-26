bfc55998d6be47379bd7e23b0e5c0b59eb6c1391
thapanabin <thapanabincr7@outlook.com>
2019-10-26 09:58:05 -0500

    getting single resources

--- src/main/java/com/nabin/springbootstarter/topic/TopicService.java
+++ src/main/java/com/nabin/springbootstarter/topic/TopicService.java
@@ -9,15 +9,19 @@ import org.springframework.stereotype.Service;
 public class TopicService {
 	
 	private List<Topic> topics = Arrays.asList(
-			new Topic("Spring","Spring Framework","Sring"),
-			new Topic("Spring","Spring Framework","Sring"),
-			new Topic("Spring","Spring Framework","Sring")
+			new Topic("spring","Spring Framework","Sring"),
+			new Topic("java","java EE Framework","java"),
+			new Topic("hibernate","hibernate Framework","hibernate")
 			
 			);
 	
 	public List<Topic> getAllTopics(){
 		
 		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
 	}
 
 }
